c56454eba205a39d69b99a5effbb3f27e5035e80
Ephraim Glick <ephraim.glick@gmail.com>
2018-01-21 06:49:52 +0000

    Switch to in-browser Mocha to avoid dependency problems

--- test/spec.js
+++ test/spec.js
@@ -1,20 +1,17 @@
-var assert = require('assert');
-var primes = require('../primes.js');
-var index = require('../index.js');
 
 describe('The getPrimes function', function() {
   it('should return the first n primes for input n', function() {
-    assert.deepEqual(primes.getPrimes(1), [2]);
-    assert.deepEqual(primes.getPrimes(5), [2, 3, 5, 7, 11]);
-    assert.deepEqual(primes.getPrimes(21), [2, 3, 5, 7, 11, 13, 17, 19, 23, 
+    assert.deepEqual(getPrimes(1), [2]);
+    assert.deepEqual(getPrimes(5), [2, 3, 5, 7, 11]);
+    assert.deepEqual(getPrimes(21), [2, 3, 5, 7, 11, 13, 17, 19, 23, 
       29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73]);
   });
 });
 
 describe('The makeTableData function', function() {
   it('should make an n x n array from an n-length input', function() {
-    let table = index.makeTableData(7);
-    assert.Equal(table.length, 7);
-    assert.Equal(table[0].length, 7);
+    let table = makeTableData(7);
+    assert.equal(table.length, 7);
+    assert.equal(table[0].length, 7);
   });
 });
