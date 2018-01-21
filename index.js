b4d4f8795fb91a400ca8a113856fcd9f723a8512
Ephraim Glick <ephraim.glick@gmail.com>
2018-01-21 06:59:17 +0000

    Complete handleClick and renderTable functions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
+function handleClick() {
+  // grab input text
+  let inputText = $("#numField").val();
+  // if input contains non-digits, reject it
+  if (!(/^\d+$/).test(inputText)) {
+    return alert('Invalid input! Please enter an integer.');
+  }
+  // turn input text into integer
+  let inputNum = Number(inputText);
+  // generate all the products for the table
+  let data = makeTableData(inputNum);
+  // create and render the table to the DOM
+  renderTable(data);
+}
+
 function makeTableData(n) {
   // use helper function from primes.js
   let primes = getPrimes(n);
@@ -12,4 +27,41 @@ function makeTableData(n) {
     }  
   }
   return data;
+}
+
+function renderTable(data) {
+  // use the n primes as column headers
+  let colHeaders = '';
+  for (let i = 0; i < data[0].length; i++) {
+    colHeaders += `<th scope="col">${primes[i]}</th>`
+  }
+  // create the table skeleton
+  let $table = $(`
+    <table>
+      <tr>
+        <th></th>
+        ${colHeaders}
+      </tr>
+    </table>
+  `);
+  // now we'll iterate through the n primes to make rows
+  for (let row = 0; row < data.length; row++) {
+    // create a row skeleton with the prime as header
+    let $row = $(`
+      <tr>
+        <th scope="row">
+          ${primes[row]}
+        </th>
+      </tr>
+    `);
+    // in the row, have a column entry for each product
+    for (let col = 0; col < data.length; col++) {
+      $row.append("<td>" + data[row][col] + "</td>");
+    }
+    // append the row to the whole table
+    $table.append($row);
+  }
+  // clear any old table away and render the new one
+  $("#primesTable").empty();
+  $("#primesTable").append($table);
 }
