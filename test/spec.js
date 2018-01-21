815ae1645fb1eb17dad7f45462e1014dc4ae297f
Ephraim Glick <ephraim.glick@gmail.com>
2018-01-21 06:26:42 +0000

    Add first test for table data generator

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var primes = require('../primes.js');
+var index = require('../index.js');
 
 describe('The getPrimes function', function() {
   it('should return the first n primes for input n', function() {
@@ -7,5 +8,13 @@ describe('The getPrimes function', function() {
     assert.deepEqual(primes.getPrimes(5), [2, 3, 5, 7, 11]);
     assert.deepEqual(primes.getPrimes(21), [2, 3, 5, 7, 11, 13, 17, 19, 23, 
       29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73]);
+  });
+});
+
+describe('The makeTableData function', function() {
+  it('should make an n x n array from an n-length input', function() {
+    let table = index.makeTableData(7);
+    assert.Equal(table.length, 7);
+    assert.Equal(table[0].length, 7);
   });
 });
