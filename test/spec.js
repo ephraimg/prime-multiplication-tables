3425b88e2617f96cde20c1c1a58725f9cf6e1c3c
Ephraim Glick <ephraim.glick@gmail.com>
2018-01-21 21:58:16 +0000

    Add test for handleClick

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,13 @@
 
-// store a known list of primes for reference in tests
-const prime100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541];
+// store an accurate list of the first 100 primes for reference in tests
+const prime100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 
+  43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 
+  109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 
+  181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 
+  257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 
+  337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 
+  419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 
+  491, 499, 503, 509, 521, 523, 541];
 
 describe('The getPrimes function', function() {
   for (let i = 0; i < 10; i++) {
@@ -12,18 +19,16 @@ describe('The getPrimes function', function() {
 });
 
 describe('The makeTableData function', function() {
+  let randSize;
   for (let i = 0; i < 10; i++) {
-    const randSize = Math.floor(Math.random() * 25);
+    randSize = 1 + Math.floor(Math.random() * 24);
     it(`should make an ${randSize} x ${randSize} array from an ${randSize}-length input`, function() {
       let table = makeTableData(randSize);
       assert.equal(table.length, randSize);
       assert.equal(table[0].length, randSize);
     });
   }
-});
-
-describe('The makeTableData function', function() {
-  const randSize = Math.floor(Math.random() * 100);
+  randSize = 1 + Math.floor(Math.random() * 99);
   for (let i = 0; i < 10; i++) {
     const num1 = Math.floor(Math.random() * randSize);
     const num2 = Math.floor(Math.random() * randSize);
@@ -35,3 +40,12 @@ describe('The makeTableData function', function() {
   }
 });
 
+let alert;
+
+describe('The handleClick function', function() {
+  it('should return null for non-integer input', function() {
+    // turn alert into noop to allow test to conclude
+    alert = () => {};
+    assert.equal(handleClick('abcd'), null);
+  });
+})
